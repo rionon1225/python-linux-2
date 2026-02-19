72b3eb66eb793e3e450f441a199e4b026325a872
rionon1225 <rionon1225@gmail.com>
2026-02-19 14:20:52 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
-# app.py
 import bottle
-from bottle import route, template
+from bottle import template
 import image
 
 app = bottle.Bottle()
@@ -9,10 +8,7 @@ def call_service():
     directoryName = "photos"
     image.process(directoryName)
 
-@app.route('/')
+@app.route("/")
 def index():
     call_service()
-    return template('index.tpl', data="Request completed", title="Image Processor App")
-
-if __name__ == "__main__":
-    bottle.run(app=app, host="0.0.0.0", port=8000)
+    return template("index.tpl", data="Request completed", title="Image Processor App")
